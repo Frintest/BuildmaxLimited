63afe2814e1761a51da4b7efa130544a313ede78
Dmitry <dimaruzaev2@gmai.com>
2022-05-28 23:02:10 +0300

    v1.2.1 - end section-gallery

--- src/public/index.js
+++ src/public/index.js
@@ -2,11 +2,10 @@ import './scss/main.scss';
 
 import './img/photos/ph_1.png';
 import './img/photos/ph_2.png';
-
 import './img/projects/pr_1.png';
 import './img/projects/pr_2.png';
 import './img/projects/pr_3.png';
-
+import './img/gallery.jpg';
 
 import './js/projects_db.js';
 
